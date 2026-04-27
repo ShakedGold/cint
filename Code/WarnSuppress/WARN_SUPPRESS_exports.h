b90f73050a1bbe0fa8e88c884453ab3fb89b2401
ShakedGold <shakedgold2005@gmail.com>
2026-04-27 23:55:41 +0300

    removed msvc in warn_suppress module

--- Code/WarnSuppress/WARN_SUPPRESS_exports.h
+++ Code/WarnSuppress/WARN_SUPPRESS_exports.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#if defined(__clang__) || defined(__GNUC__)
 #define WARN_SUPPRESS__PUSH \
     _Pragma("GCC diagnostic push")
 #define WARN_SUPPRESS__POP \
@@ -16,13 +15,3 @@
 #define WARN_SUPPRESS__IGNORE_ALL_PROTOTYPE_WARNINGS "GCC diagnostic ignored \"-Wmissing-prototypes\""
 /* convenience: ignore one warning token (exact string for _Pragma) */
 #define WARN_SUPPRESS__IGNORE(w) _Pragma(w)
-#elif defined(_MSC_VER)
-/* MSVC */
-#define WARN_SUPPRESS__PUSH __pragma(warning(push))
-#define WARN_SUPPRESS__POP __pragma(warning(pop))
-#define WARN_SUPPRESS__IGNORE(w) __pragma(warning(disable : w))
-#else
-#define WARN_SUPPRESS__PUSH
-#define WARN_SUPPRESS__POP
-#define WARN_SUPPRESS__IGNORE(w)
-#endif
